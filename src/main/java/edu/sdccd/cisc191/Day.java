83490fd2438b0eded8634189443bd17def6b6109
bernabeluigie <bernabeluigie@gmail.com>
2024-02-11 21:43:32 -0800

    initial commit

---
+++ src/main/java/edu/sdccd/cisc191/Day.java
@@ -0,0 +1,18 @@
+package edu.sdccd.cisc191;
+import java.util.Random;
+
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+
+    /**
+     * Day.getRandomDay returns a random Day of the week
+     * @return random Day
+     */
+    public static Day getRandomDay() {
+        return Day.values()[new Random().nextInt(Day.values().length)];
+    }
+
+    static final int BEGINNING_OF_WEEK = 0;
+
+
+}
--- src/main/java/edu/sdccd/cisc191/Main.java
+++ src/main/java/edu/sdccd/cisc191/Main.java
@@ -3,6 +3,30 @@ package edu.sdccd.cisc191;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Day day = Day.getRandomDay();
+        System.out.println("Today is " + day);
+
+        // Day[] days = new Day[7];
+        switch (day) {
+            case MONDAY:
+                System.out.println("yawn");
+                break;
+            case WEDNESDAY:
+                System.out.println("humpday");
+                break;
+            case FRIDAY:
+                System.out.println("party!");
+                break;
+            default:
+                System.out.println("meh");
+                break;
+        }
+
+        Week week = new Week();
+        System.out.println("Week with weekends: " + week);
+        week.removeWeekends();
+        System.out.println("Week without weekends: " + week);
+        System.out.println("Random day from this week: " + week.getRandomDay());
+
     }
 }
